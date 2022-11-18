13ce81aadb7c05e6d31a6e820176d22b0d41eaea
DanDucky <holdenwellsbennett@gmail.com>
2022-11-17 22:47:28 -0800

    late night gumpin'
    uwu

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,13 @@ use once_cell::sync::Lazy;
 // 1.3.1
 use std::sync::Mutex;
 use rocket::form::validate::Contains;
+use std::collections::HashMap;
 
 static ROW: Lazy<Mutex<Vec<u8>>> = Lazy::new(|| Mutex::new(vec![]));
 
 static PLAYER_COUNT: Lazy<Mutex<Vec<u8>>> = Lazy::new(|| Mutex::new(vec![]));
+
+static PLAYER_INDEX:Lazy<Mutex<HashMap<i32, String>>>= Lazy::new(|| Mutex::new(HashMap::new()));
 
 static FAILED_LAYERS_EVEN: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(vec![]));
 static FAILED_LAYERS_ODD: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(vec![]));
@@ -24,9 +27,30 @@ fn add() {
     ROW.lock().unwrap().push(0);
 }
 
+fn next_layer(isEven:bool) {
+    // find next possible layer based on arguments and return the id of it (MUST CHECK IF THE THING HAS ALREADY BEEN DONE)
+    // You can do this by having 2 global ints which increment by 2 each time they are selected (1 for even and 1 for odd)
+}
 
 #[get("/assign/<id>")]
 fn assign(id: i32) -> String {
+    let mut assignment:String = "0".to_string();
+    if PLAYER_INDEX.lock().unwrap()[&id].replace(".failed", "").parse::<i32>().unwrap() % 2 == 0 { // EVEN
+        if FAILED_LAYERS_EVEN.lock().unwrap().len() != 0 {
+            assignment = FAILED_LAYERS_EVEN.lock().unwrap().get(0).unwrap().to_string();
+            FAILED_LAYERS_EVEN.lock().unwrap().remove(0);
+        } else {// assign odd
+
+        }
+    } else { // ODD
+        if FAILED_LAYERS_ODD.lock().unwrap().len() != 0 {
+            assignment = FAILED_LAYERS_ODD.lock().unwrap().get(0).unwrap().to_string();
+            FAILED_LAYERS_ODD.lock().unwrap().remove(0);
+        } else { //assign even
+
+        }
+    }
+    PLAYER_INDEX.lock().unwrap().get_mut(&id).map(|val| { *val = assignment; });
     //TODO FOLLOWING LINES ARE WE NEED WRITTEN HERE IN rUsT LANG
     /*
     Check if Failed_layers.peek has anything in it && player(odd/even) is the same as Failed_Layers.peek(odd/even)
@@ -37,9 +61,10 @@ fn assign(id: i32) -> String {
 
     At bottom of function remove the file that was read from the file list and write the new file list to the text log
     ONLY DO THIS IF IT SENT A NORMAL (NOT FAILED) LAYER
+
+    TODO MOST OF WHAT IS WRITTEN ABOVE I JUST FINISHED WRITING SO DAS EPIC
      */
-    // if FAILED_LAYERS.lock().unwrap()
-    let file = File::open(format!("static/partitions/{}", ROW.lock().unwrap().len()));
+    let file = File::open(format!("static/partitions/{}", assignment));
     let reader = BufReader::new(file.unwrap());
 
     let mut lines = String::new();
@@ -76,16 +101,10 @@ fn fail_file_gen(file_name: &str, x:i32,z:i32) {
 
     if file_name.contains(".failed") {
         fs::rename(file_name, file_name.replace(".failed", "")).expect("TODO: panic message");
-        //delete file (see below)
+        fs::remove_file(format!("static/partitions/{}.failed", file_name)).expect("MEOWWWWW");
     }
-    /*
-    RIGHT HERE WE NEED TO DOO STUFF
 
-    in the above if statement delete any file named static/partitions/{file_name}.failed
-    then [OUTSIDE OF THE IF] create a new file called static/partitions/{}.failed and then create the file_out object with the same name
-     */
-
-    //create file (see above)
+    let create_file = File::create(format!("static/partitions/{}.failed", file_name)).expect("errr");
     let mut file_out = OpenOptions::new() // TODO CREATE ZE FUCKING FILE BEFORE WE TRY TO WRITE TO IT
         .write(true)
         .append(true)
