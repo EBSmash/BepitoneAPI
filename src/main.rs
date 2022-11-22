8ea297a65d67250e99469407bb710b4b70f0481f
DanDucky <holdenwellsbennett@gmail.com>
2022-11-21 16:24:44 -0800

    dewwyyyy

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,7 @@ static PLAYER_COUNT: Lazy<Mutex<Vec<u8>>> = Lazy::new(|| Mutex::new(vec![]));
 static FAILED_LAYERS_EVEN: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(vec![]));
 static FAILED_LAYERS_ODD: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(vec![]));
 
-
-static DC_LAYERS: Lazy<Mutex<HashMap<String, i32>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+static DISCONNECT_LAYERS: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(vec![]));
 
 fn next_layer(is_even: bool) -> i32 {
     let mut out: i32;
@@ -33,51 +32,67 @@ fn next_layer(is_even: bool) -> i32 {
         COUNTERS.lock().unwrap()[1] += 2;
         out = COUNTERS.lock().unwrap()[1];
     }
-    let make_file = File::create(format!("static/iterators.bep"));
+    fs::remove_file("static/iterators.bep").expect("haram");
+    File::create(format!("static/iterators.bep")).expect("halal");
     let mut iterators = OpenOptions::new()
         .write(true)
         .read(false)
-        .append(false)
+        .append(true)
         .open("static/iterators.bep")
         .unwrap();
-    writeln!(iterators, "{}", out.to_string()).expect("dewyyyy");
+    writeln!(iterators, "{}", COUNTERS.lock().unwrap()[0].to_string());
+    writeln!(iterators, "{}", COUNTERS.lock().unwrap()[1].to_string());
+
     return out;
 }
 
-fn update_failed() {}
+fn update_failed() {
+    fs::remove_file("static/failed_layers.bep").expect("reeeeee");
+    File::create("static/failed_layers.bep").expect("meow");
+    let mut failed_list = OpenOptions::new()
+        .write(true)
+        .read(false)
+        .append(true)
+        .open("static/failed_layers.bep")
+        .unwrap();
+    for line in FAILED_LAYERS_EVEN.lock().unwrap().to_vec() {
+        writeln!(failed_list,"{}", line).expect("uwu");
+    }
+    for line in FAILED_LAYERS_ODD.lock().unwrap().to_vec() {
+        writeln!(failed_list,"{}", line).expect("OWO");
+    }
+    for line in DISCONNECT_LAYERS.lock().unwrap().to_vec() {
+        writeln!(failed_list,"{}",line).expect("awa");
+    }
+}
 
 #[get("/assign/<layer>/<user>")]
 fn assign(layer: i32, user: &str) -> String {
     let mut assignment = "0".to_string();
-
-
-    if layer % 2 == 0 { // EVEN
-        if FAILED_LAYERS_EVEN.lock().unwrap().len() != 0 {
-            assignment = FAILED_LAYERS_EVEN.lock().unwrap().get(0).unwrap().to_string();
-            FAILED_LAYERS_EVEN.lock().unwrap().remove(0);
-            fs::remove_file(format!("{}.failed", assignment));
-        } else {// assign odd
-            assignment = next_layer(false).to_string();
-        }
-    } else { // ODD
-        if FAILED_LAYERS_ODD.lock().unwrap().len() != 0 {
-            assignment = FAILED_LAYERS_ODD.lock().unwrap().get(0).unwrap().to_string();
-            FAILED_LAYERS_ODD.lock().unwrap().remove(0);
-            fs::remove_file(format!("{}.failed", assignment));
-            //TODO update the FAILED_LAYERS text file (maybe make function to do this?)
-        } else { //assign even
-            assignment = next_layer(true).to_string();
-        }
-    }
-
-    if DC_LAYERS.lock().unwrap().len() != 0 {
-        if DC_LAYERS.lock().unwrap().contains_key(user) {
-            assignment = DC_LAYERS.lock().unwrap().get(user).unwrap().to_string();
-            fs::remove_file(format!("{}.disconnected", assignment));
-            DC_LAYERS.lock().unwrap().remove(user);
+    if DISCONNECT_LAYERS.lock().unwrap().to_vec().contains(user.clone().to_string()) {
+        DISCONNECT_LAYERS.lock().unwrap().retain(|value| *value != user);
+        assignment = user.to_string();
+    } else {
+        if layer % 2 == 0 { // EVEN
+            if FAILED_LAYERS_EVEN.lock().unwrap().len() != 0 {
+                assignment = FAILED_LAYERS_EVEN.lock().unwrap().get(0).unwrap().to_string();
+                FAILED_LAYERS_EVEN.lock().unwrap().remove(0);
+                // fs::remove_file(format!("{}.failed", assignment));
+            } else {// assign odd
+                assignment = next_layer(false).to_string();
+            }
+        } else { // ODD
+            if FAILED_LAYERS_ODD.lock().unwrap().len() != 0 {
+                assignment = FAILED_LAYERS_ODD.lock().unwrap().get(0).unwrap().to_string();
+                FAILED_LAYERS_ODD.lock().unwrap().remove(0);
+                // fs::remove_file(format!("{}.failed", assignment));
+                //TODO update the FAILED_LAYERS text file (maybe make function to do this?)
+            } else { //assign even
+                assignment = next_layer(true).to_string();
+            }
         }
     }
-
+    update_failed();
 
     let file = File::open(format!("static/partitions/{}", &*assignment));
     let reader = BufReader::new(file.unwrap());
@@ -90,8 +105,8 @@ fn assign(layer: i32, user: &str) -> String {
     return lines.to_string();
 }
 
-#[get("/fail/<file_name>/<x>/<z>")]
-fn fail_file_gen(file_name: &str, x: i32, z: i32) {
+#[get("/fail/<file_name>/<x>/<y>/<z>/<name>")]
+fn fail_file_gen(file_name: &str, x: i32, y:i32, z: i32, name: String) {
     let file = File::open(format!("static/partitions/{}", file_name));
 
     let reader = BufReader::new(file.unwrap());
@@ -110,37 +125,50 @@ fn fail_file_gen(file_name: &str, x: i32, z: i32) {
         fs::rename(file_name, file_name.replace(".failed", "")).expect("TODO: panic message");
         fs::remove_file(format!("static/partitions/{}.failed", file_name)).expect("MEOWWWWW");
     }
-    let _create_file = File::create(format!("static/partitions/{}.failed", file_name)).expect("errr");
-    let mut file_out = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(format!("static/partitions/{}.failed", file_name))
-        .unwrap();
-    if let Err(e) = write!(file_out, "{}.failed\n", format!("{}", lines.get(0).unwrap())) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
+    if y != 256 {
+        File::create(format!("static/partitions/{}.failed", file_name)).expect("errr");
+        let mut file_out = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("static/partitions/{}.failed", file_name))
+            .unwrap();
+        if let Err(e) = write!(file_out, "{}", format!("{}", lines.get(0).unwrap())) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
 
-    if file_name.parse::<i32>().unwrap() % 2 == 0 { //even
-        FAILED_LAYERS_EVEN.lock().unwrap().push(format!("{}.failed", file_name));
+        if file_name.parse::<i32>().unwrap() % 2 == 0 { //even
+            FAILED_LAYERS_EVEN.lock().unwrap().push(format!("{}.failed", file_name));
+        } else {
+            FAILED_LAYERS_ODD.lock().unwrap().push(format!("{}.failed", file_name));
+        }
+
+        for lineNum in line_err - 1..lines.len() {
+            let current = lines.get(lineNum).unwrap();
+
+            writeln!(file_out, "{}", current.to_string()).expect("failed to write");
+            println!("{}", current);
+        }
+        update_failed()
+        //TODO then copy the current QUEUE to the queue log text file
     } else {
-        FAILED_LAYERS_ODD.lock().unwrap().push(format!("{}.failed", file_name));
+        File::create(format!("static/partitions/{}", name)).expect("errr");
+        let mut file_out = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("static/partitions/{}", name))
+            .unwrap();
+        if let Err(e) = write!(file_out, "{}", format!("{}\n", file_name)) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+        DISCONNECT_LAYERS.lock().unwrap().push(name.clone());
+        for lineNum in line_err - 1..lines.len() {
+            let current = lines.get(lineNum).unwrap();
+
+            writeln!(file_out, "{}", current.to_string()).expect("failed to write");
+            println!("{}", current);
+        }
+        update_failed();
     }
-
-    for lineNum in line_err - 1..lines.len() {
-        let current = lines.get(lineNum).unwrap();
-
-        writeln!(file_out, "{}", current.to_string()).expect("failed to write");
-        println!("{}", current);
-    }
-
-    let mut failed_list = OpenOptions::new()
-        .write(true)
-        .read(false)
-        .append(true)
-        .open("static/failed_layers.bep")
-        .unwrap();
-    writeln!(failed_list, "{}.failed", file_name).expect("MEOWWWW");
-    //TODO then copy the current QUEUE to the queue log text file
 }
 
 #[get("/start")]
@@ -156,66 +184,36 @@ fn end() {
     PLAYER_COUNT.lock().unwrap().remove(0);
 }
 
-
-#[get("/dc/<file_name>/<x>/<z>/<username>")]
-fn disconnect_file_gen(file_name: &str, x: i32, z: i32, username: &str) {
-    let file = File::open(format!("static/partitions/{}", file_name));
-
-    let reader = BufReader::new(file.unwrap());
-
-    let mut lines = vec![];
-    let mut line_err = 0;
-    for mut line in reader.lines() {
-        let formatted_line = format!("{} {}", x, z);
-        let line = line.unwrap().clone();
-        lines.push(line.clone());
-        if line.clone().as_str().contains(formatted_line.as_str()) {
-            line_err = lines.len()
-        }
-    }
-    if file_name.contains(".failed") {
-        fs::rename(file_name, file_name.replace(".disconnected", "")).expect("TODO: panic message");
-        fs::remove_file(format!("static/partitions/{}.disconnected", file_name)).expect("MEOWWWWW");
-    }
-    let _create_file = File::create(format!("static/partitions/{}.disconnected", file_name)).expect("errr");
-    let mut file_out = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(format!("static/partitions/{}.disconnected", file_name))
-        .unwrap();
-    if let Err(e) = write!(file_out, "{}.disconnected.{}\n", format!("{}", lines.get(0).unwrap()), username) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
-
-    DC_LAYERS.lock().unwrap().insert(username.to_string(), file_name.parse::<i32>().unwrap());
-
-    if lines.len() > 0 {
-        for lineNum in line_err..lines.len() {
-            let current = lines.get(lineNum).unwrap();
-
-            writeln!(file_out, "{}", current.to_string()).expect("failed to write");
-            println!("{}", current);
-        }
-    }
-}
-
-
 #[launch]
 fn rocket() -> _ { // idk but this fixed shit
 
-
-    let file = File::open("static/partitions/iterators.bep");
+    let file = File::open("static/iterators.bep");
 
     let reader = BufReader::new(file.unwrap());
-
+    let mut iter = 0;
     for line in reader.lines() {
-        if line.unwrap().parse::<i32>() {
-            for _ in 0..line.unwrap().parse::<i32>() {
-                COUNTERS.lock().unwrap().push(0)
+        // if line.unwrap().parse::<i32>() {
+        //     for _ in 0..line.unwrap().parse::<i32>() {
+        //         COUNTERS.lock().unwrap().push(0)
+        //     }
+        // }
+        COUNTERS.lock().unwrap()[iter] = line.unwrap().parse::<i32>().expect("owo");
+        iter += 1;
+    }
+    let file = File::open("static/failed_layers.bep");
+    let reader = BufReader::new(file.unwrap());
+    for line in reader.lines() {
+        if line.as_ref().unwrap().contains(".failed") {
+            if line.as_ref().unwrap().split(".").collect::<Vec<&str>>()[0].parse::<i32>().expect("meow") % 2 == 0 {
+                FAILED_LAYERS_EVEN.lock().unwrap().push(line.unwrap().to_string());
+            } else {
+                FAILED_LAYERS_ODD.lock().unwrap().push(line.unwrap().to_string());
             }
+        } else {
+            DISCONNECT_LAYERS.lock().unwrap().push(line.unwrap().to_string());
         }
     }
 
-    rocket::build().mount("/", routes![assign, start, end, fail_file_gen, disconnect_file_gen])
+    rocket::build().mount("/", routes![assign, start, end, fail_file_gen])
 
 }
