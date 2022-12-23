7ef2a04c2aec5b2812d89eadae6cff4a32f5bb75
Babbaj <babbaj45@gmail.com>
2022-12-23 03:14:50 -0500

    always return our assignment regardless of parity

--- src/main.rs
+++ src/main.rs
@@ -55,8 +55,7 @@ fn choose_existing_assignment(con: &Connection, user: &str, is_even: bool) -> ru
         SELECT username,assignments.layer
         FROM assignments
         JOIN layers ON assignments.layer = layers.layer AND layers.finished = 0 -- only if the layer is unfinished
-        WHERE (username = :user OR UNIXEPOCH() - last_update > 86400) -- 24 hours
-        AND assignments.layer % 2 = :parity
+        WHERE (username = :user OR (UNIXEPOCH() - last_update > 86400 AND assignments.layer % 2 = :parity)) -- 24 hours
         ORDER BY IIF(username = :user, 0, 1) -- us first
         LIMIT 1
     "};
